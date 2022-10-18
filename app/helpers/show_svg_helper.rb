778196009e332aec6755f9647e72cb629d2cd110
ftambara <98750668+ftambara@users.noreply.github.com>
2022-10-18 20:21:22 -0300

    Replace img tag with svg via new helper function

---
+++ app/helpers/show_svg_helper.rb
@@ -0,0 +1,13 @@
+module ShowSvgHelper
+  def show_svg(path, classes="")
+    content = File.open("app/assets/images/#{path}", "rb") do |file|
+      content = file.read
+      unless classes.empty?
+        content.sub! /<svg[^>]+>/ do |svg_tag|
+          "#{svg_tag.chop} class=\"#{classes}\">"
+        end
+      end
+    end
+    raw content
+  end
+end
