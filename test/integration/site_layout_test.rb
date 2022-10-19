4ba5ac22408a1162e2682388d33b6f881e1fbbdd
ftambara <98750668+ftambara@users.noreply.github.com>
2022-10-18 21:49:13 -0300

    Add pending notice to SiteLayoutTest

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,5 +4,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "navbar links" do
     get root_path
     assert_select "a[href=?]", root_path, count: 1
+    skip "Missing session links tests"
   end
 end
