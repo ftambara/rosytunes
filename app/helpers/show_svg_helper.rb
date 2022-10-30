85a812bc88652b04c932b14d3bb05dc89ac0b6d2
ftambara <98750668+ftambara@users.noreply.github.com>
2022-10-30 11:24:05 -0300

    Refactor show_svg helper

--- app/helpers/show_svg_helper.rb
+++ app/helpers/show_svg_helper.rb
@@ -1,13 +1,20 @@
 module ShowSvgHelper
-  def show_svg(path, classes = "")
-    content = File.open("app/assets/images/#{path}", "rb") do |file|
-      content = file.read
-      unless classes.empty?
-        content.sub! /<svg[^>]+>/ do |svg_tag|
-          "#{svg_tag.chop} class=\"#{classes}\">"
-        end
+  def show_svg(name, classes = [])
+    svg = File.open("app/assets/images/#{name}", "rb") do |file|
+      append_to_tag(file.read, class_attr(classes), "svg") unless classes.empty?
+    end
+    # Don't escape tags
+    raw svg
+  end
+
+  private
+    def class_attr(classes)
+      %Q{class="#{classes.join(" ")}"}
+    end
+
+    def append_to_tag(source, appendage, tag)
+      source.sub!(/<#{tag}[^>]+>/) do |element|
+        "#{element.chop} #{appendage}\">"
       end
     end
-    raw content
-  end
 end
