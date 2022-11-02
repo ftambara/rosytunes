1c0edd66ac7b6e17b56ae5f2c4149e679ed2697b
ftambara <98750668+ftambara@users.noreply.github.com>
2022-11-01 21:58:19 -0300

    Configure mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -69,4 +68,14 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["ROSY_EMAIL_USERNAME"],
+    password:             ENV["ROSY_EMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 end
