a96796b5d2042983301162485c2d9ef7fa97862e
ftambara <98750668+ftambara@users.noreply.github.com>
2022-11-15 16:45:06 -0300

    Fix system tests PENDING improve performance

--- test/system/finding_albums_test.rb
+++ test/system/finding_albums_test.rb
@@ -4,12 +4,13 @@ class FindingAlbumsTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
 
   test "searching for an album" do
-    sign_in(users(:one))
     visit root_path
 
     select "Albums", from: "Entity"
     fill_in "Query", with: "Appetite For Destruction"
+
     click_on "Search"
+    sleep(10)
 
     find(".album_card", match: :first).click_link
   end
