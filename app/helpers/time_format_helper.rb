438d484c67fccd2a3fd342580c664838002cb603
ftambara <98750668+ftambara@users.noreply.github.com>
2022-11-17 15:41:59 -0300

    Rework album show view

---
+++ app/helpers/time_format_helper.rb
@@ -0,0 +1,9 @@
+module TimeFormatHelper
+  def format_duration(time_in_s)
+    hours = (time_in_s / (60 * 60)).floor
+    minutes = ((time_in_s / 60) % 60).floor
+    seconds = time_in_s % 60
+    [(hours if hours > 0), minutes, seconds].compact
+      .map { |n| "%02d" % n}.join(":")
+  end
+end
