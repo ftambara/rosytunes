3288d49194f1c338d62a1415c183dbad1982c864
ftambara <98750668+ftambara@users.noreply.github.com>
2022-11-24 10:55:32 -0300

    Delete unnecesary instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
-    unless @user.id == params[:id].to_i
+    unless current_user.id == params[:id].to_i
       redirect_back_or_to root_path,
         notice: "For the moment, user profile pages are private"
     end
